38058878b08d4174b35af2d876565141f8b21422
Jared <jlhfqaz@gmail.com>
2021-08-09 14:16:30 +0800

    initial commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+const token = process.env.TELEGRAM_TOKEN;
+var telegramUrl = "https://api.telegram.org/bot" + token;
+
+const axios = require('axios');
+
+console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
+
+//setWebhook https://api.telegram.org/bot<token>/setWebhook?url=<url>
+
+function processUpdate(body) {
+  sendText(body.message.chat.id, body.message.text);
+}
+
+function sendText(chatId, text) {
+  var data = {
+    "chat_id": chatId,
+    "text": text,
+    "parse_mode": "Markdown",
+  };
+  const res = axios.post(`${telegramUrl}/sendMessage`, data);
+  res.then(response => {
+    return response;
+  }).catch(err => console.log(err));
+}
+
+module.exports.processUpdate = processUpdate;
