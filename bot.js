bdb3f47f5ed6470d26907cb3c75d431d535880aa
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-14 23:02:01 +0800

    trying

--- bot.js
+++ bot.js
@@ -8,16 +8,22 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  sendText(body.message.chat.id,body.message.image);
+  sendPhoto(body.message.chat.id,body.message.text);
 }
-
-function sendText(chatId, image) {
+function sendPhoto(chatId,text){
   var data = {
     "chat_id": chatId,
-    "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg": image,
+    "text": text,
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AArrow-right-small.svg&psig=AOvVaw2kN0giXxl4UuyWjcggL1yf&ust=1631717969095000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDJ_aPd_vICFQAAAAAdAAAAABAD":"image",
     "parse_mode": "Markdown",
-  };
-  const res = axios.post(`${telegramUrl}/sendMessage`, data);
+}
+// function sendText(chatId, text) {
+//   var data = {
+//     "chat_id": chatId,
+//     "text": text,
+//     "parse_mode": "Markdown",
+//   };
+  const res = axios.post(`${telegramUrl}/sendPhoto`, data);
   res.then(response => {
     return response;
   }).catch(err => console.log(err));
