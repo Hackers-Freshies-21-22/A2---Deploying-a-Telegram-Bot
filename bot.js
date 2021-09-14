08912ef2940ac3b9a0979549f4d61d5feddc51aa
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-14 20:17:36 +0800

    fixing

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  var alllowercase = body.message.text.toLowercase();
+  var alllowercase = body.message.text.toLowerCase();
   var splitstring = alllowercase.split("");
   var reversestring = splitstring.reverse();
   var joinstring = reversestring.join("");
