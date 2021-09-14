f636e42d5b0db15e5d0ace11926793c1f7111f85
yyj-02 <yeohyongjie@outlook.com>
2021-09-15 01:20:54 +0800

    Photo

--- bot.js
+++ bot.js
@@ -48,7 +48,9 @@ function sendPhoto(chatId, photo) {
   const token = "https://api.telegram.org/bot1978424816:AAFG8d6tFpLg_Hx22bBl-AD_CInMDyeQcbs/getFile?file_id=";
   const url = token.concat(photo[0].file_id);
   // "https://api.telegram.org/file/bot1978424816:AAFG8d6tFpLg_Hx22bBl-AD_CInMDyeQcbs/".concat(url.result.file_path),
-  const obj = axios.get(url).then(response => response.data);
+  const promise = await axios.get(url)
+  const obj = promise.then((response) => response.data)
+  // const obj = axios.get(url).then(response => response.data);
   var data = {
     "chat_id": chatId,
     "text" : obj,
