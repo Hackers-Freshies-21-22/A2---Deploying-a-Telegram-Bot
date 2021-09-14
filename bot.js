948bbdcb7bb6fd2b589d7d4edda3ff2848b8223c
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-15 01:29:03 +0800

    trying

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  getFile(body.message.Photosize.id);
+  getFile(body.message.PhotoSize.file_id);
 }
 
 function getFile(fileId) {
