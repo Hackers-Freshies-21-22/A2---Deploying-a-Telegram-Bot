38aca1e47d10f62b4e3b3de5e1c139db97dfafb8
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-14 23:35:16 +0800

    trying

--- bot.js
+++ bot.js
@@ -2,20 +2,20 @@ const token = process.env.TELEGRAM_TOKEN;
 var telegramUrl = "https://api.telegram.org/bot" + token;
 
 const axios = require('axios');
+const { text } = require('express');
 
 console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  sendText(body.message.chat.id,body.message.text);
+  sendPhoto(body.message.chat.id,body.message.photo);
 }
-
-function sendPhoto(chatId, text) {
+function sendPhoto(chatId, photo) {
   var data = {
     "chat_id": chatId,
-    "text": text,
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2F1mb&psig=AOvVaw07_qHM_Vt2Okeon233hW0I&ust=1631718248467000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIi8pKbe_vICFQAAAAAdAAAAABAD":"image",
+    " https://telegram.org/img/t_logo.png": photo,
+    "text":text,
     "parse_mode": "Markdown",
   };
   const res = axios.post(`${telegramUrl}/sendPhoto`, data);
@@ -23,5 +23,16 @@ function sendPhoto(chatId, text) {
     return response;
   }).catch(err => console.log(err));
 }
+// function sendText(chatId, text) {
+//   var data = {
+//     "chat_id": chatId,
+//     "text": text,
+//     "parse_mode": "Markdown",
+//   };
+//   const res = axios.post(`${telegramUrl}/sendMessage`, data);
+//   res.then(response => {
+//     return response;
+//   }).catch(err => console.log(err));
+// }
 
 module.exports.processUpdate = processUpdate;
