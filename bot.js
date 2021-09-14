4ebf4b8d2925d75e9a7bc9c021975ffe6517741b
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-14 23:15:10 +0800

    trying

--- bot.js
+++ bot.js
@@ -8,25 +8,20 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  sendPhoto(body.message.chat.id,body.message.text);
+  sendText(body.message.chat.id,body.message.text);
 }
-function sendPhoto(chatId,text){
+
+function sendPhoto(chatId, text) {
   var data = {
     "chat_id": chatId,
     "text": text,
     "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2F1mb&psig=AOvVaw07_qHM_Vt2Okeon233hW0I&ust=1631718248467000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIi8pKbe_vICFQAAAAAdAAAAABAD":"image",
-    "parse_mode": "Markdown",}
-}
-// function sendText(chatId, text) {
-//   var data = {
-//     "chat_id": chatId,
-//     "text": text,
-//     "parse_mode": "Markdown",
-//   };
+    "parse_mode": "Markdown",
+  };
   const res = axios.post(`${telegramUrl}/sendPhoto`, data);
   res.then(response => {
     return response;
   }).catch(err => console.log(err));
-
+}
 
 module.exports.processUpdate = processUpdate;
