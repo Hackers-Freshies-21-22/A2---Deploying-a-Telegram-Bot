c7f5350937dfbd678fc877c2801a98f7af4eaaaf
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-14 20:07:29 +0800

    changes to accomadate lower and upper cases

--- bot.js
+++ bot.js
@@ -8,10 +8,10 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  var splitstring = body.message.text.split("");
+  var splitstring = body.message.text.toLowercase().split("");
   var reversestring = splitstring.reverse();
   var joinstring = reversestring.join("");
-  if (body.message.text===joinstring){
+  if (body.message.text.toLowercase()===joinstring){
   sendText(body.message.chat.id,"true");}
   else {sendText(body.message.chat.id,"false");}
 }
