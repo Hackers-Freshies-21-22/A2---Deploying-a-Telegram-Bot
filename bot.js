69576e62e6fa1992b8bc613406a607ba00901b45
Yuvaraj Kumaresan <yuvaraj408@gmail.com>
2021-09-15 22:42:44 +0800

    tying part 4

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 //setWebhook https://api.telegram.org/bot1928355698:AAGhQC-xDoTW0zxgxDQuQ6xayjACQEDDFyQ/setWebhook?url=https://immense-escarpment-36150.herokuapp.com/
 
 function processUpdate(body) {
-  var file_id = body.message.photo[0].file_id;
+  var file_id = body["message"]["photo"][0]["file_id"];
   return sendEchoPhoto(body.message.chat.id,body.message.photo);
 }
 function sendEchoPhoto(chatId, file_id){
